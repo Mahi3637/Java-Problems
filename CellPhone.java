db7bb53b0b7582f53a168d851511f2cdfecef360
unknown <studentmdmausufhossain@gmail.com>
2025-08-14 03:52:09 +0600

    conyact

--- CellPhone.java
+++ CellPhone.java
@@ -1,10 +1,32 @@
-public class CellPhone{
-    public String model;
-    public String storeContact;
-    public int [] contacts = new int [0];
+public class CellPhone {
+public String [] contacts;
+public int contactCount;
+public String model;
 
-    public CellPhone(String model, String storeContact){
-        this.model = "unknown";
-        this.storeContact = "Not set";
+public CellPhone(){
+   this.model = "Unknown";
+   this.contactCount = 0;
+   this.contacts = new String [3];
+}
+
+public void storeContact(String contact){
+    if(contactCount < 3){
+        contacts[contactCount] = contact;
+        contactCount++;
     }
+    else{
+        System.out.println("Memory is full and  cannot add more of the contyacts");
+    }
+}
+
+public void printDetails(){
+    System.out.println("Phone Model: "+ model);
+    System.out.println("Contact Count: "+ contactCount);
+    System.out.println("The contacts are: ");
+    if(contactCount > 0){
+        for(int i = 0; i<contactCount; i++){
+           System.out.println(contacts[i]);
+        }
+    }
+}
 }
---
+++ Test1Tester.java
@@ -0,0 +1,9 @@
+public class Test1Tester {
+    public static void main (String args[]){
+        Test1 t1 = new Test1();
+        t1.methodB(5,-8);
+        Test1 t2 = new Test1();
+        t2.methodA();
+      }
+    
+}
