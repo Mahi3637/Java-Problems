0956f9529ab45a8a75e8813bcbcaa0a734e4aef6
unknown <studentmdmausufhossain@gmail.com>
2025-08-17 20:28:48 +0600

    Static Tracing

---
+++ FinalT6ATester.java
@@ -0,0 +1,10 @@
+public class FinalT6ATester {
+    public static void main(String [] args){
+        FinalT6A q1 = new FinalT6A(2,1);
+        q1.methodA();
+        FinalT6A q2 = new FinalT6A(3,1);
+        q2.methodB(7,8);
+     }
+
+
+}
---
+++ TracingTester.java
@@ -0,0 +1,12 @@
+public class TracingTester {
+    public static void main(String [] args){
+        Tracing t1= new Tracing(2, 3);
+        t1.methodA(1);
+        Tracing t2= new Tracing(3, 4);
+        t2.methodA(2);
+        t1.methodB(t2);
+        t2.methodB(t2);
+    }
+
+
+}
