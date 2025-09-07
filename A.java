acb5870375273b34733f1aadbe7c72e61a890104
unknown <studentmdmausufhossain@gmail.com>
2025-09-08 02:47:23 +0600

    CSE110 After mid tracing

--- A.java
+++ A.java
@@ -1,22 +1,39 @@
-public class A {
-    public int temp = 4;
-    public int sum, y, x;
-    public void methodA(int m){
-        int [] n = {2,5};
+public class A{
+    public static int temp = 3;
+    public int sum;
+    public int y;
+    public A(){
+        y = temp - 1;
+        sum = temp + 2;
+        temp-=2;
+    }
+    public void methodA(int m, int [] n){
         int x = 0;
-        y = m + this.methodB(x++,m)+(temp++);
-        x = this.x + 2 + n[0];
+        y = y + m + (temp++);
+        x = x + 2 +  (++n[0]);
         sum = sum + x + y;
         n[0] = sum + 2;
-        System.out.println(n[0]+" " + x+ " " + sum);
+        System.out.println(x + " " + y+ " " + sum);
     }
-    public int methodB(int m, int n){
-        int y = 4 + this.y + m;
-        x = this.y + y + (++temp) - n;
-        sum = x + y + this.sum;
-        System.out.println(y+ " " + this.x + " " +sum);
-        return x;
-   }
- }
- 
- 
+}
+class B extends A {
+    public static int x = 1;
+    public B(){
+        y = temp + 1 ;
+        x = 3 + temp + x;
+        temp-=2;
+    }  
+    public B(B b){
+        sum = b.sum + super.sum;
+        x = b.x + x;
+    }
+    public void methodB(int m, int n){    
+       int [] y = {0};
+        super.y = y[0] + this.y + m; 
+        x = super.y + 2 + temp - n;
+        methodA(x, y);
+        sum = x + y[0] + super.sum;
+        System.out.println(x + " " + y[0]+ " " + sum);
+    }
+}
+
--- ATester.java
+++ ATester.java
@@ -1,14 +1,17 @@
-
-public class ATester {
+public class ATester{
     public static void main(String [] args){
-        A t1 = new A();
-        t1.methodA(5);
-        A t2 = new A();
-        t2.methodB(12, 2);
+        int x[] = {23};
+A a1 = new A();
+B b1 = new B();
+B b2 = new B(b1);
+a1.methodA(1, x);
+b2.methodB(3, 2);
+a1.methodA(1, x);
+
+
+
+
+
+
     }
- }
- 
- 
- 
- 
- 
+}
